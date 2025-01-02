42f9b95ab086cc29e5c9b8ffd68488c03f9135d8
Austin Merrill <austinm9506@gmail.com>
2025-01-02 01:54:05 -0500

    Finally resolved my 404 error by adding in aliases for ajmerr.com and www.ajmerr.com to my CloudFront distribution.

--- src/main.tf
+++ src/main.tf
@@ -68,13 +68,23 @@ resource "aws_s3_bucket_ownership_controls" "ajmerr_host_ownership" {
 # AWS CloudFront 
 resource "aws_cloudfront_distribution" "ajmerr_distribution" {
   origin {
-    domain_name = aws_s3_bucket.ajmerr_host.bucket_regional_domain_name
+    domain_name = "${aws_s3_bucket.ajmerr_host.bucket}.s3-website-${var.aws_region}.amazonaws.com"
     origin_id   = "S3-${aws_s3_bucket.ajmerr_host.id}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
+
+  # Creates aliases for my domain
+  aliases = ["ajmerr.com", "www.ajmerr.com"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
